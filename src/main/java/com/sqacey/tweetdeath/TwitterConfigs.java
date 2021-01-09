a5f133e9b0f57363b0b7c788667e7b5e1d13d3ad
real-afluegge <32827316+real-afluegge@users.noreply.github.com>
2021-01-09 14:45:49 -0500

    Update TwitterConfigs.java

--- src/main/java/com/sqacey/tweetdeath/TwitterConfigs.java
+++ src/main/java/com/sqacey/tweetdeath/TwitterConfigs.java
@@ -9,10 +9,10 @@ public class TwitterConfigs {
 
         ConfigurationBuilder cb = new ConfigurationBuilder();
         cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("xCeFnOA15lRTiHxa4QJiJHBzk")
-                .setOAuthConsumerSecret("b16RDd1hvA4grG1MUIIZl7mmG950tRJdkXYoFcC7fQ5FQgm2O0")
-                .setOAuthAccessToken("2802523530-bqq1Z2or5u6GuoYB99BDbWK6I2GRv5fciebRV66")
-                .setOAuthAccessTokenSecret("mHqXY8Fq2tjrNwQodkvT6bYeFseKxicxX44JhJQCTOw4A");
+                .setOAuthConsumerKey("key")
+                .setOAuthConsumerSecret("secret")
+                .setOAuthAccessToken("token")
+                .setOAuthAccessTokenSecret("secret");
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
 
